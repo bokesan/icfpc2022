860d062f1b7e403677d19ba4e5d9772aaae15a3a
Christoph Breitkopf <chbreitkopf@gmail.com>
2022-09-03 14:12:33 +0200

    Fix unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -34,13 +34,16 @@ unitTests = testGroup "Unit tests"
     testCase "mixColors" $
       mixColors (PixelRGBA8 20 40 60 80) (PixelRGBA8 10 20 30 6) @?= (PixelRGBA8 15 30 45 43)
   , testCase "format line cut" $
-      show (LineCut (Block "1.0.2" undefined) Vertical 13) @?= "cut [1.0.2] [X] [13]"
+      show (LineCut (blk "1.0.2") Vertical 13) @?= "cut [1.0.2] [X] [13]"
   , testCase "format point cut" $
-      show (PointCut (Block "2.1" undefined) 12 15) @?= "cut [2.1] [12,15]"
+      show (PointCut (blk "2.1") 12 15) @?= "cut [2.1] [12,15]"
   , testCase "format color" $
-      show (Color (Block "3" undefined) (makeColor 1 2 3 4)) @?= "color [3] [1,2,3,4]"
+      show (Color (blk "3") (makeColor 1 2 3 4)) @?= "color [3] [1,2,3,4]"
   , testCase "format swap" $
-      show (Swap (Block "4.0" undefined) (Block "2" undefined)) @?= "swap [4.0] [2]"
+      show (Swap (blk "4.0") (blk "2")) @?= "swap [4.0] [2]"
   , testCase "format merge" $
-      show (Merge (Block "4.1" undefined) (Block "2.2" undefined)) @?= "merge [4.1] [2.2]"
+      show (Merge (blk "4.1") (blk "2.2")) @?= "merge [4.1] [2.2]"
   ]
+
+blk :: String -> Block
+blk id1 = Block id1 (Rectangle 0 0 0 0)
